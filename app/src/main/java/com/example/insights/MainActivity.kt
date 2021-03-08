18bb68c1afd243533246ab2de340ee023b027b6f
Amarnath K <amarnathk5674@gmail.com>
2021-03-08 19:52:37 +0530

    bug fix

--- app/src/main/java/com/example/insights/MainActivity.kt
+++ app/src/main/java/com/example/insights/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         Handler().postDelayed(
             {
                 startActivity(Intent(this,LoginPage::class.java))
+                finish()
             },
             2500
         )
