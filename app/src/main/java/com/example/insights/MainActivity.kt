539d01c218e8abc56ba41139246bbac07b62ab24
Amarnath K <amarnathk5674@gmail.com>
2021-03-09 22:44:15 +0530

    update for progress bar(not complete)

--- app/src/main/java/com/example/insights/MainActivity.kt
+++ app/src/main/java/com/example/insights/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.insights
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +21,6 @@ class MainActivity : AppCompatActivity() {
             2500
         )
     }
+
 
 }
