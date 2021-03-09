c5ae0fd9cb456e46b8d1adc518eb762649a6498e
Amarnath K <amarnathk5674@gmail.com>
2021-03-09 08:14:28 +0530

    Forgot_password page in progress

--- app/src/main/java/com/example/insights/LoginPage.kt
+++ app/src/main/java/com/example/insights/LoginPage.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import android.widget.Toast.makeText as makeText1
@@ -13,6 +14,11 @@ class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        val forgotBtn = findViewById<TextView>(R.id.tvForgotPassword)
+        forgotBtn.setOnClickListener {
+            startActivity(Intent(this@LoginPage,ForgotPasswordActivity::class.java))
+            finish()
+        }
         val loginBtn = findViewById<Button>(R.id.loginButton)
         loginBtn.setOnClickListener {
             val email = findViewById<EditText>(R.id.login_email_id).text.toString()
@@ -21,11 +27,7 @@ class LoginPage : AppCompatActivity() {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password).addOnCompleteListener {task ->
                 if(task.isSuccessful){
                     Toast.makeText(this@LoginPage,"You have been logged in.",Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this@LoginPage,testhomeActivity2::class.java)
-                    startActivity(intent)
-                    intent.putExtra("User_Id",FirebaseAuth.getInstance().currentUser!!.uid)
-                    intent.putExtra("email_id",email)
-                    finish()
+
                 }
                 else{
                     Toast.makeText(this@LoginPage,"Login Failed.",Toast.LENGTH_SHORT).show()
@@ -33,5 +35,6 @@ class LoginPage : AppCompatActivity() {
             }
         }
     }
+
 
 }
