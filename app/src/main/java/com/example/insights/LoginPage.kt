4984f3c639aad6dd6f2498f5c9e5379e5ea16f91
Amarnath K <amarnathk5674@gmail.com>
2021-03-10 08:14:58 +0530

    Connected Login-Junction-signup pages

--- app/src/main/java/com/example/insights/LoginPage.kt
+++ app/src/main/java/com/example/insights/LoginPage.kt
@@ -1,12 +1,17 @@
 package com.example.insights
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+    }
+    fun openJunction(view: View){
+        startActivity(Intent(this@LoginPage,Junction::class.java))
     }
 
 }
--- app/src/main/java/com/example/insights/MainActivity.kt
+++ app/src/main/java/com/example/insights/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.insights
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this@MainActivity,LoginPage::class.java))
+            finish()
+        }, 2500)
+
     }
-    fun startLoginActivity(view:View){
-        val intent = Intent(this,LoginPage::class.java)
-        startActivity(intent)
-        finish()
-    }
+
 }
