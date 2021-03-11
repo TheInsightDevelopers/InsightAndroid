81e3376aa0674af1c197c2755fcaf69ef2aedab2
Amarnath K <amarnathk5674@gmail.com>
2021-03-11 21:38:01 +0530

    made student data to be stored

--- app/src/main/java/com/example/insights/Student.kt
+++ app/src/main/java/com/example/insights/Student.kt
@@ -7,6 +7,9 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class Student : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +18,32 @@ class Student : AppCompatActivity() {
         findViewById<Button>(R.id.student_register_btn).setOnClickListener {
             val email = findViewById<EditText>(R.id.student_register_email).text.toString()
             val password = findViewById<EditText>(R.id.student_register_password).text.toString()
+            val name = findViewById<EditText>(R.id.student_register_name).text.toString()
+            val classname = findViewById<EditText>(R.id.student_register_class).text.toString()
+            val school = findViewById<EditText>(R.id.student_register_school).text.toString()
+            val favSubject = findViewById<EditText>(R.id.student_register_favSubject).text.toString()
+            val data = hashMapOf(
+                "Name" to name,
+                "Class" to classname,
+                "Fav_subject" to favSubject,
+                "School" to school
+            )
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener{ task ->
                 if(task.isSuccessful){
                     Toast.makeText(this@Student,"You have been registered successfully",Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this@Student,Main_page::class.java))
-                    finish()
+                    val db = FirebaseFirestore.getInstance()
+                    db.collection("User_students").document(FirebaseAuth.getInstance().currentUser.uid).set(data).addOnCompleteListener { task1 ->
+                        if(task1.isSuccessful){
+                            Toast.makeText(this@Student,"Your details have saved successfully",Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this@Student,Main_page::class.java))
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this@Student,"An error occurred",Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
                 }
                 else{
                     Toast.makeText(this@Student,"Registration Failed",Toast.LENGTH_SHORT).show()
