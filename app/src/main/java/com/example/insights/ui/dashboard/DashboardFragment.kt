a04800f2766c4d71273af3311379f7ab8de748e8
ash-100 <vashwinjino@gmal.com>
2021-03-11 23:57:42 +0530

    new main page has been made

---
+++ app/src/main/java/com/example/insights/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,27 @@
+package com.example.insights.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.insights.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        return root
+    }
+}
