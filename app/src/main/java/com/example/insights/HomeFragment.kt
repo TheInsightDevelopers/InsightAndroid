56b58202c80cdb92b79d5312c8eb537a2a85d949
ash-100 <vashwinjino@gmal.com>
2021-03-15 00:51:02 +0530

    main page has been updated

--- app/src/main/java/com/example/insights/HomeFragment.kt
+++ app/src/main/java/com/example/insights/HomeFragment.kt
@@ -1,5 +1,31 @@
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.example.insights.R
+import com.example.insights.androidDevelopment1
+import java.util.zip.Inflater
 
-class HomeFragment:Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val imageButton1 : ImageButton =root.findViewById(R.id.androidDevelopmentImageButton1)
+        imageButton1.setOnClickListener{
+            val intent = Intent(activity,androidDevelopment1::class.java)
+            startActivity(intent)
+        }
+
+        return root
+    }
 }
