8c555697ea17a05b049e8c97bbe25e6f1389d853
Amarnath K <amarnathk5674@gmail.com>
2021-03-20 07:02:48 +0530

    Made till sent option

--- app/src/main/java/com/example/insights/ForumnActivity.kt
+++ app/src/main/java/com/example/insights/ForumnActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.crashlytics.internal.common.CurrentTimeProvider
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
@@ -13,6 +15,7 @@ class ForumnActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forumn)
+        /***************************************Sending Message***********************************************/
         //adding onclick listener
         findViewById<ImageButton>(R.id.forum_send_button).setOnClickListener {
             //getting message
@@ -20,13 +23,16 @@ class ForumnActivity : AppCompatActivity() {
             if(message.isNotEmpty()){
                 var current_time = Date().time.toString()
                 var data = hashMapOf("message" to message,
-                                        "time" to current_time)
-                // Sending Message to firestore
+                                        "time" to current_time,
+                                            "sender" to FirebaseAuth.getInstance().currentUser.email)
+                // Sending Message to Firestore
                 FirebaseFirestore.getInstance().collection("message").document(current_time).set(data)
 
             }
             findViewById<EditText>(R.id.message_input).text.clear()
         }
     }
+    /***************************************Listener Message***********************************************/
+    
 
 }
