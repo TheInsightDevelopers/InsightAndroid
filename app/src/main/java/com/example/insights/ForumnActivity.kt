4b06bab83c78a21788303d954f81541b59550d7d
Amarnath K <amarnathk5674@gmail.com>
2021-03-21 15:29:53 +0530

    Made recycler

--- app/src/main/java/com/example/insights/ForumnActivity.kt
+++ app/src/main/java/com/example/insights/ForumnActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.insights.adapters.forumadapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.crashlytics.internal.common.CurrentTimeProvider
 import com.google.firebase.firestore.DocumentSnapshot
@@ -15,6 +18,11 @@ class ForumnActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forumn)
+        /*****************************************************Recycler View*****************************************************/
+        val recyclerview = findViewById<RecyclerView>(R.id.chat_recycler)
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        val Itemadapter = forumadapter(this,getItemsList())
+        recyclerview.adapter = Itemadapter
         /***************************************Sending Message***********************************************/
         //adding onclick listener
         findViewById<ImageButton>(R.id.forum_send_button).setOnClickListener {
@@ -32,7 +40,14 @@ class ForumnActivity : AppCompatActivity() {
             findViewById<EditText>(R.id.message_input).text.clear()
         }
     }
-    /***************************************Listener Message***********************************************/
-    
+    private fun getItemsList(): ArrayList<HashMap<String,String>> {
+        val list = ArrayList<HashMap<String,String>>()
+        for(i in 1..15){
+            list.add(hashMapOf("sender" to "User ${i}",
+                "message" to "Hi  ${i}"))
+        }
+        return list
+    }
+
 
 }
---
+++ app/src/main/java/com/example/insights/adapters/forumadapter.kt
@@ -0,0 +1,41 @@
+package com.example.insights.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.insights.R
+
+
+class forumadapter(val context: Context, val items: ArrayList<HashMap<String,String>>):
+    RecyclerView.Adapter<forumadapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.forum_custom_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item =  items.get(position)
+        holder.tv_item.text = item["message"]
+        holder.sender.text = item["sender"]
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tv_item = view.findViewById<TextView>(R.id.message)
+
+        val sender = view.findViewById<TextView>(R.id.forum_sender_tv)
+    }
+
+}
