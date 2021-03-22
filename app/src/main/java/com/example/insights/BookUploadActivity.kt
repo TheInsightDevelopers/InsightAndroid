97c80e6fdfbf0f7baa5d7ae9eadc4dd5adc55eaa
Amarnath K <amarnathk5674@gmail.com>
2021-03-22 15:59:24 +0530

    Made layout for book upload

---
+++ app/src/main/java/com/example/insights/BookUploadActivity.kt
@@ -0,0 +1,11 @@
+package com.example.insights
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookUploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_upload)
+    }
+}
