d17beda9ca873e6267db9fbc33a089deff250d4f
Amarnath K <amarnathk5674@gmail.com>
2021-03-24 12:48:50 +0530

    Buggy version

--- app/src/main/java/com/example/insights/HomeFragment.kt
+++ app/src/main/java/com/example/insights/HomeFragment.kt
@@ -1,4 +1,6 @@
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,29 +21,37 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+
         val bookvalues = ArrayList<HashMap<String,String>>()
-        val db = FirebaseFirestore.getInstance()
-        db.collection("Books").get().addOnSuccessListener { books ->
-            for(book in books){
-                bookvalues.add(book.data as HashMap<String, String>)
-            }
-        }
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
         var recyclerView: RecyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
         recyclerView.layoutManager= LinearLayoutManager(context)
-        val itemAdapter= ItemAdapter(this,bookvalues)
+        val itemAdapter= ItemAdapter(this,getItems())
         recyclerView.adapter=(itemAdapter)
 
         return root
     }
-    private fun getItemsList(): ArrayList<String> {
-        val list = ArrayList<String>()
 
-        for(i in 1..15){
-            list.add("Item $i")
+
+    private fun getItems(): ArrayList<HashMap<String,String>> {
+        val testhashmap = hashMapOf("Title" to "Amar's Books")
+        FirebaseFirestore.getInstance().collection("Books").document("testBook").set(testhashmap).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+            }
         }
+            val bookslist = ArrayList<HashMap<String,String>>()
 
-        return list
+            FirebaseFirestore.getInstance().collection("Books").get().addOnSuccessListener { books ->
+                for(book in books){
+                    bookslist.add(book.data as HashMap<String, String>)
+                }
+            }.addOnFailureListener { exception ->
+                Log.d(TAG,"Error in getting documents", exception)
+            }
+            bookslist.add(hashMapOf("Title" to "Sample book"))
+        return bookslist
     }
+
 }
--- app/src/main/java/com/example/insights/ItemAdapter.kt
+++ app/src/main/java/com/example/insights/ItemAdapter.kt
@@ -1,18 +1,14 @@
 package com.example.insights
 
 import HomeFragment
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.recyclerview.widget.RecyclerView
-import java.security.AccessController.getContext
 
-class ItemAdapter(val context: HomeFragment, val items:ArrayList<HashMap<String,String>>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(val context: HomeFragment, val items: ArrayList<HashMap<String,String>>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.home_custom_row,parent,false))
     }
