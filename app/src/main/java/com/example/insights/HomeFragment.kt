0687afc6b709fcd6fcc5776f4e73e267bfecd206
Amarnath K <amarnathk5674@gmail.com>
2021-03-28 10:34:00 +0530

    Bug fixed, Fix #46

--- app/src/main/java/com/example/insights/HomeFragment.kt
+++ app/src/main/java/com/example/insights/HomeFragment.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.insights.ItemAdapter
 import com.example.insights.R
+import com.example.insights.adapters.forumadapter
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeFragment : Fragment() {
@@ -28,30 +30,29 @@ class HomeFragment : Fragment() {
 
         var recyclerView: RecyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
         recyclerView.layoutManager= LinearLayoutManager(context)
-        val itemAdapter= ItemAdapter(this,getItems())
+        val itemAdapter= ItemAdapter(this,bookvalues)
         recyclerView.adapter=(itemAdapter)
+        val testhashmap = hashMapOf("Title" to "Amar's Books")
+        val bookslist = ArrayList<HashMap<String,String>>()
+        val db = FirebaseFirestore.getInstance()
+        db.collection("Books").addSnapshotListener { value, error ->
+            if(error== null){
+                if (value != null) {
+                    for (change in value!!.documentChanges){
+                        if(change.type == DocumentChange.Type.ADDED){
+                            bookvalues.add(change.document.data as HashMap<String, String>)
+                            val Itemadapter = ItemAdapter(this, bookvalues)
+                            recyclerView.adapter = Itemadapter
+                        }
+                    }
+                }
+            }
+        }
 
         return root
     }
 
 
-    private fun getItems(): ArrayList<HashMap<String,String>> {
-        val testhashmap = hashMapOf("Title" to "Amar's Books")
-        FirebaseFirestore.getInstance().collection("Books").document("testBook").set(testhashmap).addOnCompleteListener { task ->
-            if(task.isSuccessful){
-            }
-        }
-            val bookslist = ArrayList<HashMap<String,String>>()
 
-            FirebaseFirestore.getInstance().collection("Books").get().addOnSuccessListener { books ->
-                for(book in books){
-                    bookslist.add(book.data as HashMap<String, String>)
-                }
-            }.addOnFailureListener { exception ->
-                Log.d(TAG,"Error in getting documents", exception)
-            }
-            bookslist.add(hashMapOf("Title" to "Sample book"))
-        return bookslist
-    }
 
 }
