0c76975e2d563f2ae48fd7ec14a61115e2112b86
ash-100 <vashwinjino@gmal.com>
2021-03-28 19:19:11 +0530

    on click listener has been added to recycler view

--- app/src/main/java/com/example/insights/BookFragment.kt
+++ app/src/main/java/com/example/insights/BookFragment.kt
@@ -1,5 +1,50 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.insights.ItemAdapter
 import com.example.insights.R
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
-class BookFragment:Fragment(R.layout.fragment_book) {
+class BookFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        val bookvalues = ArrayList<HashMap<String,String>>()
+
+        val root = inflater.inflate(R.layout.fragment_book, container, false)
+
+        var recyclerView: RecyclerView = root.findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager= LinearLayoutManager(context)
+        val itemAdapter= ItemAdapter(this,bookvalues)
+        recyclerView.adapter=(itemAdapter)
+        val db = FirebaseFirestore.getInstance()
+        db.collection("Books").addSnapshotListener { value, error ->
+            if(error== null){
+                if (value != null) {
+                    for (change in value!!.documentChanges){
+                        if(change.type == DocumentChange.Type.ADDED){
+                            bookvalues.add(change.document.data as HashMap<String, String>)
+                            val Itemadapter = ItemAdapter(this, bookvalues)
+                            recyclerView.adapter = Itemadapter
+                        }
+                    }
+                }
+            }
+        }
+
+        return root
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/insights/ItemAdapter.kt
+++ app/src/main/java/com/example/insights/ItemAdapter.kt
@@ -1,14 +1,21 @@
 package com.example.insights
 
-import HomeFragment
+import BookFragment
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
-class ItemAdapter(val context: HomeFragment, val items: ArrayList<HashMap<String,String>>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(val context: BookFragment, val items: ArrayList<HashMap<String,String>>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.home_custom_row,parent,false))
     }
@@ -22,10 +29,27 @@ class ItemAdapter(val context: HomeFragment, val items: ArrayList<HashMap<String
     override fun getItemCount(): Int {
         return items.size
     }
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
-
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        lateinit var uriImage: Uri
+        lateinit var uriPdf: Uri
         val topicItem: TextView =view.findViewById(R.id.topic_name)
         val cardViewItem: CardView =view.findViewById(R.id.card_view_item)
+        init {
+            view.setOnClickListener { v:View->
+                val position:Int=adapterPosition
+                val intent= Intent(Intent.ACTION_VIEW)
+                intent.setType("application/pdf")
+
+                //uncomment the below one when you do
+                // intent.setData()
+
+
+
+            }
+        }
 
     }
+
 }
+
+
