cdfce4c01bda174f414fadea7cc82351d35bbf70
Amarnath K <amarnathk5674@gmail.com>
2021-03-29 16:48:11 +0530

    Made buttons activie

--- app/src/main/java/com/example/insights/InstructorProfileActivity.kt
+++ app/src/main/java/com/example/insights/InstructorProfileActivity.kt
@@ -1,10 +1,12 @@
 package com.example.insights
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -12,7 +14,45 @@ class InstructorProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructor_profile_page)
-        findViewById<Button>(R.id.startEditInstructor).setOnClickListener {
+        findViewById<Button>(R.id.instructor_profile_uploadvideo).setOnClickListener {
+            startActivity(Intent(this@InstructorProfileActivity,UploadVideoActivity::class.java))
+        }
+        findViewById<Button>(R.id.instructor_profile_uploadbook).setOnClickListener {
+            startActivity(Intent(this@InstructorProfileActivity,BookUploadActivity::class.java))
+        }
+        findViewById<Button>(R.id.instructor_profile_signout).setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this@InstructorProfileActivity,LoginPage::class.java))
+            finish()
+            Toast.makeText(this@InstructorProfileActivity,"You have been Logged Out", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.instructor_profile_deleteaccount).setOnClickListener {
+            val progressBar = Dialog(this)
+            progressBar.setContentView(R.layout.progress_bar)
+            progressBar.show()
+            FirebaseFirestore.getInstance().collection("Users").document(FirebaseAuth.getInstance().currentUser.uid).delete().addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    FirebaseAuth.getInstance().currentUser.delete().addOnCompleteListener { task2 ->
+                        if(task2.isSuccessful){
+                            progressBar.hide()
+                            Toast.makeText(this@InstructorProfileActivity,"Your account and data has been deleted",Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this@InstructorProfileActivity,LoginPage::class.java))
+                            finish()
+                        }
+                        else{
+                            progressBar.hide()
+                            Toast.makeText(this@InstructorProfileActivity,"An error Occurred",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(this@InstructorProfileActivity,"An error Occured",Toast.LENGTH_SHORT).show()
+                    progressBar.hide()
+
+                }
+            }
+        }
+        findViewById<Button>(R.id.instructor_profile_editprofile).setOnClickListener {
             startActivity(Intent(this@InstructorProfileActivity,InstructorEditProfileActivity::class.java))
         }
         val name_tv = findViewById<TextView>(R.id.instructor_profile_name_tv)
