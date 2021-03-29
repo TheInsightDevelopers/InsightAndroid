364b61a87088b0414aba484196c511b1020df5bb
Amarnath K <amarnathk5674@gmail.com>
2021-03-29 11:02:48 +0530

    made firebase data for pdf

--- app/src/main/java/com/example/insights/ItemAdapter.kt
+++ app/src/main/java/com/example/insights/ItemAdapter.kt
@@ -10,26 +10,36 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.io.File
+import java.net.URL
 
 class ItemAdapter(val context: BookFragment, val items: ArrayList<HashMap<String,String>>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    lateinit var url: String
+    lateinit var uri: Uri
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemAdapter.ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.home_custom_row,parent,false))
     }
-
     override fun onBindViewHolder(holder: ItemAdapter.ViewHolder, position: Int) {
         val item=items.get(position)
         holder.topicItem.text=item["Book Name"]
+        url = item["PdfUrl"].toString()
+        val dataReference = FirebaseStorage.getInstance().getReferenceFromUrl(url)
+        val localFile = File.createTempFile("Book","Pdf")
+        uri = Uri.fromFile(localFile)
+        dataReference.getFile(localFile).addOnSuccessListener {
 
+        }
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         lateinit var uriImage: Uri
         lateinit var uriPdf: Uri
         val topicItem: TextView =view.findViewById(R.id.topic_name)
@@ -40,8 +50,7 @@ class ItemAdapter(val context: BookFragment, val items: ArrayList<HashMap<String
                 val intent= Intent(Intent.ACTION_VIEW)
                 intent.setType("application/pdf")
 
-                //uncomment the below one when you do
-                // intent.setData()
+                intent.setData(uri)
 
 
 
@@ -49,6 +58,7 @@ class ItemAdapter(val context: BookFragment, val items: ArrayList<HashMap<String
         }
 
     }
+
 
 }
 
